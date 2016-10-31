d3b865c8be2dbfd9a0b2bfb5e6c5f21ab8ca146b
srich020 <srich020@mtroyal.ca>
2016-10-31 14:34:41 -0600

    First update test

--- Term Project COMP 3504/Term Project COMP 3504/Resources/Resource.Designer.cs
+++ Term Project COMP 3504/Term Project COMP 3504/Resources/Resource.Designer.cs
@@ -57,6 +57,22 @@ namespace Term_Project_COMP_3504
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int switch1 = 2131034112;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			

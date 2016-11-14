51d9213ace82393c0b122781519e9cae8ed92000
srich020 <srich020@mtroyal.ca>
2016-11-14 14:19:47 -0700

    Activity 2 Deleted.

--- SampleGridView1/HelloGridView/Resources/Resource.Designer.cs
+++ SampleGridView1/HelloGridView/Resources/Resource.Designer.cs
@@ -84,29 +84,32 @@ namespace HelloGridView
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050004
-			public const int MMtitle = 2131034116;
+			// aapt resource value: 0x7f050005
+			public const int MMtitle = 2131034117;
 			
 			// aapt resource value: 0x7f050001
 			public const int button1 = 2131034113;
 			
-			// aapt resource value: 0x7f050002
-			public const int button2 = 2131034114;
-			
 			// aapt resource value: 0x7f050003
-			public const int gridview = 2131034115;
+			public const int button2 = 2131034115;
 			
-			// aapt resource value: 0x7f050007
-			public const int optionsButton = 2131034119;
+			// aapt resource value: 0x7f050004
+			public const int gridview = 2131034116;
 			
-			// aapt resource value: 0x7f050005
-			public const int playButton = 2131034117;
+			// aapt resource value: 0x7f050008
+			public const int optionsButton = 2131034120;
 			
 			// aapt resource value: 0x7f050006
-			public const int scoreButton = 2131034118;
+			public const int playButton = 2131034118;
+			
+			// aapt resource value: 0x7f050007
+			public const int scoreButton = 2131034119;
 			
 			// aapt resource value: 0x7f050000
 			public const int textView1 = 2131034112;
+			
+			// aapt resource value: 0x7f050002
+			public const int textview = 2131034114;
 			
 			static Id()
 			{
